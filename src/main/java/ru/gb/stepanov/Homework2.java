b400b9b8f016f4c14de678af90f2a7f1ba44b28e
StepPasha <pphudc@inbox.ru>
2022-04-19 21:26:15 +0300

    Task4and5

--- src/main/java/ru/gb/stepanov/Homework2.java
+++ src/main/java/ru/gb/stepanov/Homework2.java
@@ -7,8 +7,7 @@ public class Homework2 {
         methodTaskTwo(10);
         boolean method;
         method = methodPositiveOrNegative(90);
-        method4("Hello", 3);
-        methodLeapYear(2016);
+
 
     }
 
@@ -42,22 +41,8 @@ public class Homework2 {
             return true;
         }
     }
-
-    public static void method4(String msg, int count) {
-        for (int i = 0; i < count; i++) {
-
-            System.out.println(msg);
-        }
-    }
-
-    public static boolean methodLeapYear(int year) {
-        if ((year % 4 == 0) && (year % 100 != 0) || (year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
-            System.out.println("true");
-            return true;
-        } else {
-            System.out.println("false");
-            return false;
-        }
-    }
 }
+
+
+
 
